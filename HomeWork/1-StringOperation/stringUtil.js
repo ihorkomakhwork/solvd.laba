682ca550f0b2312aa783725cf32f8c7c09ffb92f
Komakh Ihor Ivanovich <I.Komakh.FIT.122.20@knute.edu.ua>
2023-07-17 11:54:31 +0000

    add stringUtil.test

--- HomeWork/1-StringOperation/stringUtil.js
+++ HomeWork/1-StringOperation/stringUtil.js
@@ -1,34 +1,94 @@
-class Calculator{
-    constructor(strA, strB) {
-        this.a = parseInt(strA, 10);
-        this.b = parseInt(strB, 10);
-    }
-    
-    plus() { 
-        const sum = this.a + this.b;
-        return sum.toString();
-    }
+const plus = (argA, argB) => {
+  let result = '';
+  let carry = 0;
+  let i = argA.length - 1;
+  let j = argB.length - 1;
 
-    minus () {
-        const sum = this.a - this.b;
-        return sum.toString();
-    
-    } 
-    
-    divide () {
-        const sum = this.a / this.b;
-        return sum.toString();
-    }
-    
-    multiply () {
-        const sum = this.a * this.b;
-        return sum.toString();
-    } 
+  while (i >= 0 || j >= 0 || carry !== 0) {
+
+    const digitA = i >= 0 ? parseInt(argA[i]) : 0;
+    const digitB = j >= 0 ? parseInt(argB[j]) : 0;
+    const sum = digitA + digitB + carry;
+
+    result = (sum % 10) + result;
+    carry = Math.floor(sum / 10);
+
+    i--;
+    j--;
+  }
+  return result;
 }
 
-const calac = new Calculator('2', '2');
+const minus = (num1, num2) => {
+  let result = '';
+  let borrow = 0;
+  let i = num1.length - 1;
+  let j = num2.length - 1;
 
-console.log(calac.plus());
-console.log(calac.minus());
-console.log(calac.divide());
-console.log(calac.multiply());
+  while (i >= 0 || j >= 0) {
+    const digit1 = i >= 0 ? parseInt(num1[i]) : 0;
+    const digit2 = j >= 0 ? parseInt(num2[j]) : 0;
+
+    let diff = digit1 - borrow - digit2;
+
+    if (diff < 0) {
+      diff += 10;
+      borrow = 1; 
+    } else {
+      borrow = 0;
+    }
+
+    result = diff + result;
+    i--;
+    j--;
+  }
+  result = result.replace(/^0+/, '');
+  return result || '0';
+};
+ 
+const multiply = (num1, num2) => {
+    const m = num1.length;
+    const n = num2.length;
+    const result = Array(m + n).fill(0);
+  
+    for (let i = m - 1; i >= 0; i--) {
+      for (let j = n - 1; j >= 0; j--) {
+        const product = parseInt(num1[i]) * parseInt(num2[j]);
+        const p1 = i + j;
+        const p2 = i + j + 1;
+        const sum = product + result[p2];
+  
+        result[p1] += Math.floor(sum / 10);
+        result[p2] = sum % 10;
+      }
+    }
+  
+    while (result.length > 1 && result[0] === 0) {
+      result.shift();
+    }
+  
+    return result.join('');
+};
+
+
+const divide = (str1, str2) => {
+  let result = "";
+  let remainder = 0;
+  let i = 0;
+  while (i < str1.length) {
+      let dividend = remainder * 10 + parseInt(str1[i]);
+      let quotient = Math.floor(dividend / str2);
+      remainder = dividend % str2;
+      result += quotient;
+      i++;
+  }
+  result = result.replace(/^0+/, '');
+  return result || '0';
+};
+
+module.exports = { 
+  plus, 
+  minus, 
+  multiply, 
+  divide, 
+};
---
+++ HomeWork/1-StringOperation/stringUtil.test.js
@@ -0,0 +1,72 @@
+const assert = require('node:assert');
+const {describe, it} = require('node:test');
+const stringUtil = require('./stringUtil');
+
+describe('Plus', () => {
+    it('Two ints', () => {
+        const sum = stringUtil.plus('5', '5');
+        assert.strictEqual(sum, '10');
+    });
+
+    it('Big num and standart int', () => {
+        const sum = stringUtil.plus('9988877799887788999887889998789877899888777998877889998878899987898778', '1');
+        assert.strictEqual(sum, '9988877799887788999887889998789877899888777998877889998878899987898779');
+    });
+
+    it('Two big num', () => {
+        const sum = stringUtil.plus('9988877799887788999887889998789877899888777998877889998878899987898778', '9988877799887788999887889998789877899888777998877889998878899987898778');
+        assert.strictEqual(sum, '19977755599775577999775779997579755799777555997755779997757799975797556');
+    });
+}); 
+
+
+describe('Minus', () => {
+    it('Two ints', () => {
+        const difference = stringUtil.minus('235', '5');
+        assert.strictEqual(difference, '230');
+    });
+
+    it('Big num and standart int', () => {
+        const difference = stringUtil.minus('9988877799887788999887889998789877899888777998877889998878899987898778', '1');
+        assert.strictEqual(difference, '9988877799887788999887889998789877899888777998877889998878899987898777');
+    });
+
+    it('Two big num', () => {
+        const difference = stringUtil.minus('9988877799887788999887889998789877899888777998877889998878899987898778', '9988877799887788999887889998789877899888777998877889998878899987898778');
+        assert.strictEqual(difference, '0');
+    });
+}); 
+  
+describe('Multiply', () => {
+    it('Two ints', () => {
+        const product = stringUtil.multiply('5', '5');
+        assert.strictEqual(product, '25');
+    });
+
+    it('Big num and standart int', () => {
+        const product = stringUtil.multiply('9988877799887788999887889998789877899888777998877889998878899987898778', '1');
+        assert.strictEqual(product, '9988877799887788999887889998789877899888777998877889998878899987898778');
+    });
+
+    it('Two big num', () => {
+        const product = stringUtil.multiply('9988877799887788999887889998789877899888777998877889998878899987898778', '9988877799887788999887889998789877899888777998877889998878899987898778');
+        assert.strictEqual(product, '99777679701091116064141285104054636388523420718562054883974097820025537837164504043906278437096425184120239296982669673973360114839573893284');
+    });
+});
+  
+describe('Divide', () => {
+    it('Two ints', () => {
+        const quotient = stringUtil.divide('25', '5');
+        assert.strictEqual(quotient, '5');
+    });
+
+    it('Big num and standart int', () => {
+        const quotient = stringUtil.divide('9988877799887788999887889998789877899888777998877889998878899987898778', '1');
+        assert.strictEqual(quotient, '9988877799887788999887889998789877899888777998877889998878899987898778');
+    });
+
+    it('Two big num', () => {
+        const quotient = stringUtil.divide('9988877799887788999887889998789877899888777998877889998878899987898778', '9988877799887788999887889998789877899888777998877889998878899987898778');
+        assert.strictEqual(quotient, '1');
+    });
+});
