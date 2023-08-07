af7d34a1a3b21976a1b1943aeab1b89f6752cf06
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2023-08-07 20:47:59 +0300

    add gettersAndSetters

---
+++ HomeWork/4-Objects/3-gettersAndSetters.js
@@ -0,0 +1,41 @@
+const bankAccount = {
+    ownwerName: 'John Wick',
+    _balance:  1000,
+    get formatedBalance() {
+        return `$${this._balance}`;
+    },
+    get balance() {
+        return this._balance;
+    },
+    set balance(value) {
+        this._balance = value;
+    }, 
+    transfer(amount, account) {
+        if (this.balance < amount) throw new Error('Insufficient funds');
+        this.balance = this.balance - amount;
+        account.balance = account.balance + amount;
+    }
+};
+
+const bankAccount2 = {
+    ownwerName: 'Blade Runner',
+    _balance:  300,
+    get formatedBalance() {
+        return `$${this._balance}`;
+    },
+    get balance() {
+        return this._balance;
+    },
+    
+    set balance(value) {
+        this._balance = value;
+    }, 
+    transfer(amount, account) {
+        if(this.balance < amount) throw new Error('Insufficient funds');
+        this.balance -= amount;
+        account.balance += amount;
+    }
+};
+
+bankAccount.transfer(100, bankAccount2);
+console.log(bankAccount2.formatedBalance);
