d98e2d12b7ddf5c88a0f8841bd906c86d3c3756f
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2023-09-07 23:16:17 +0300

    fix promiseAll

--- HomeWork/2-Block/3-Promises/1-promiseAll.ts
+++ HomeWork/2-Block/3-Promises/1-promiseAll.ts
@@ -1,11 +1,13 @@
 export const promiseAll = (promises: Promise<any>[]): Promise<any[]> => new Promise((resolve, reject) => {
-        const result: any[] = [];
-        let counter = 0;
-        promises.forEach((promise, index) => {
-            promise.then((res) => {
-                result[index] = res;
-                counter++;
-                if (counter === promises.length) resolve(result);
-            }).catch((err) => reject(err));
+    promises.reduce((acc, promise) => {
+        return promise.then(value => {
+                             return acc.then(state => { 
+                                return [...state, value] 
+                            }
+                            )
+                    }).catch(error => reject(error))}, Promise.resolve([])) 
+        
+        .then(finall=> resolve(finall))
+    
+
         });
-});
