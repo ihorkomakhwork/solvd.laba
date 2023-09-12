1238e48c925a7cdfbb56d4b8da20ecc9cc97a080
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2023-09-12 13:38:10 +0300

    Remove garbage

--- HomeWork/2-Block/3-Promises/3-chainPromises.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chainPromises = void 0;
-const chainPromises = (promises) => promises
-    .reduce((acc, func) => acc = acc
-    .then(func)
-    .catch(error => error), Promise.resolve());
-exports.chainPromises = chainPromises;
-//# sourceMappingURL=3-chainPromises.js.map
--- HomeWork/2-Block/4-Classes/app/core/book.ts
+++ HomeWork/2-Block/4-Classes/app/core/book.ts
@@ -1,9 +1,7 @@
 import { CRUD } from '../util/crud';
 import { IBook } from './interfaces/book.interface';
 
-// Book class extends CRUD class and implements IBook interface 
 export class Book extends CRUD<IBook> {
-    // setTable method set db table name and in future we will use it in CRUD class
-    setTable(): string { return 'book'; }
+     setTable(): string { return 'book'; }
     constructor( ) {super();}
 }
